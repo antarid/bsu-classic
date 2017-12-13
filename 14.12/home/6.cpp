3eea1b616e9b01eee195a45c6cae34141d605d60
Viktor <anashkinviktar@gmail.com>
2017-12-14 00:54:30 +0300

    14.12 hometask done

--- 14.12/home/6.cpp
+++ 14.12/home/6.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 int main () {
     int n,
-        temp,
         digit;
     cin >> n;
-    cout << "n(hex) = " << hex << n << endl;
+    cout << "n (hex) = " << hex << n << endl;
     bool flag = false;
-    for ( int k=28, i=0; k>=0; k-=4, i++ ) {
-        temp = n>>k;
-        digit = temp&15;
+    for ( int k=268435456; k>=1; k /= 16 ) {
+        digit = (n/k)%16;
         if ( digit!=0 ) flag = true;
         if ( flag ) {
             cout << digit << " ";
--- 14.12/home/7.cpp
+++ 14.12/home/7.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 int main () {
-    int n;
+    int n,
+        digit;
     cin >> n;
-    cout << "n (hex) = " << hex << n << endl;
+    cout << "n(hex) = " << hex << n << endl;
+    bool flag = false;
+    for ( int k=28; k>=0; k-=4) {
+        digit = (n>>k)&15;
+        if ( digit!=0 ) flag = true;
+        if ( flag ) {
+            cout << digit << " ";
+        }
+    }
+    cout << endl;
 }
